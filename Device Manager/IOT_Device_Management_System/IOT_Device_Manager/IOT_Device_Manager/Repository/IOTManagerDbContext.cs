b9a0f83f42e38cd7190ddc3eda2044790fee7ee1
Tjaart Pelser <tjaart2000@gmail.com>
2022-09-04 17:34:44 +0200

    Conn String Patch
    Edited Connection String to DB

--- Device Manager/IOT_Device_Management_System/IOT_Device_Manager/IOT_Device_Manager/Repository/IOTManagerDbContext.cs
+++ Device Manager/IOT_Device_Management_System/IOT_Device_Manager/IOT_Device_Manager/Repository/IOTManagerDbContext.cs
@@ -28,8 +28,7 @@ namespace IOT_Device_Manager.Repository
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=c323-big-iot-server.database.windows.net;Initial Catalog=IOT-Device-Manager;User ID=Drakensburg;Password=Cypher128");
+                optionsBuilder.UseSqlServer("Data Source=c323-big-iot-server.database.windows.net;Initial Catalog=IOT-Device-Manager;User ID=Drakensburg;Password=***********");
             }
         }
 
