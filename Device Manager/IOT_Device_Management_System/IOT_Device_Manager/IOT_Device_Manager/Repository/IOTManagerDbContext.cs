61ad5e3044b891c8f9a81acff26df15631efc87d
Tjaart Pelser <tjaart2000@gmail.com>
2022-09-04 16:35:13 +0200

    Project Setup Complete
    03/09/22 - Project Setup							- Complete
    		   |Create the API project					- Complete
    		   |Connect the API to the data source		- Complete

---
+++ Device Manager/IOT_Device_Management_System/IOT_Device_Manager/IOT_Device_Manager/Repository/IOTManagerDbContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using IOT_Device_Manager.Repository.Models;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace IOT_Device_Manager.Repository
+{
+    public partial class IOTManagerDbContext : DbContext
+    {
+        public IOTManagerDbContext()
+        {
+        }
+
+        public IOTManagerDbContext(DbContextOptions<IOTManagerDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Category { get; set; }
+        public virtual DbSet<Device> Device { get; set; }
+        public virtual DbSet<Zone> Zone { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=c323-big-iot-server.database.windows.net;Initial Catalog=IOT-Device-Manager;User ID=Drakensburg;Password=Cypher128");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.CategoryId).ValueGeneratedNever();
+
+                entity.Property(e => e.DateCreated).HasDefaultValueSql("(getdate())");
+            });
+
+            modelBuilder.Entity<Device>(entity =>
+            {
+                entity.Property(e => e.DeviceId).ValueGeneratedNever();
+
+                entity.Property(e => e.DateCreated).HasDefaultValueSql("(getdate())");
+            });
+
+            modelBuilder.Entity<Zone>(entity =>
+            {
+                entity.Property(e => e.ZoneId).ValueGeneratedNever();
+
+                entity.Property(e => e.DateCreated).HasDefaultValueSql("(getdate())");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
--- Device Manager/IOT_Device_Management_System/IOT_Device_Manager/IOT_Device_Manager/Startup.cs
+++ Device Manager/IOT_Device_Management_System/IOT_Device_Manager/IOT_Device_Manager/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IOT_Device_Manager.Repository.Models;
 
 namespace IOT_Device_Manager
 {
