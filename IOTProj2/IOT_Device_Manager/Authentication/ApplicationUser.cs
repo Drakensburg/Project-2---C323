20fdd3fd16d09c93ddeb4d57ba8b4e60ab14930d
Tjaart Pelser <tjaart2000@gmail.com>
2022-09-07 02:57:04 +0200

    Authentication Setup Complete
    Security allows for Authorization of Users

---
+++ IOTProj2/IOT_Device_Manager/Authentication/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace IOT_Device_Manager.Authentication
+{
+    public class ApplicationUser: IdentityUser
+    {
+
+    }
+}
--- IOTProj2/IOT_Device_Manager/Startup.cs
+++ IOTProj2/IOT_Device_Manager/Startup.cs
@@ -13,6 +13,17 @@ using System.Threading.Tasks;
 using IOT_Device_Manager.Repository.Models;
 using IOT_Device_Manager.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using IOT_Device_Manager.Authentication;
 
 namespace IOT_Device_Manager
 {
@@ -31,10 +42,42 @@ namespace IOT_Device_Manager
             services.AddControllers();
             services.AddSwaggerGen(options => { options.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "IOT Device Manager", Version = "v2", Description = "Device Manager Program for an Interconnected Office Environment", }); });
             services.AddDbContext<IOTManagerDbContext>(options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
+
+            // For Entity Framework  
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ConnStr")));
+
+            // For Identity  
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Adding Authentication  
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+
+            // Adding Jwt Bearer  
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = Configuration["JWT:ValidAudience"],
+                    ValidIssuer = Configuration["JWT:ValidIssuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
+                };
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -45,13 +88,14 @@ namespace IOT_Device_Manager
 
             app.UseRouting();
 
+            app.UseAuthentication();  
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
-            app.UseSwagger(); 
+            app.UseSwagger();
             app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v2/swagger.json", "IOT Device Manager"));
         }
     }
